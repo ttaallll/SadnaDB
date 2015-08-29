bccdf232a89a8dd681134f3f330b2bb5aee6f116
talpais <pais@tals-MacBook-Pro.local>
2015-08-29 16:25:23 +0300

    fix data mining

--- dataMining.py
+++ dataMining.py
@@ -9,7 +9,7 @@ def getDataMining(rc):
     query = 'select b.id, b.title, w1.id, w1.word, w2.id, w2.word, dm.apperanceCount, dm.dateCreated ' \
             'from sadnadb.dataMining dm, sadnadb.books b, sadnadb.words w1, sadnadb.words w2 ' \
             'where b.id = dm.bookId and w1.id = dm.wordId1 and w2.id = dm.wordId2 ' \
-            'ORDER BY b.id, dm.apperanceCount DESC LIMIT 1000'
+            'ORDER BY dm.apperanceCount DESC LIMIT 2000'
     cursor.execute(query)
     result = cursor.fetchall()
 
@@ -36,12 +36,13 @@ def getDataMining(rc):
 def runDataMining(rc, bookId):
 
     cursor = rc["db"].cursor()
-    query = 'insert into sadnadb.dataMining (bookId, apperanceCount, wordId1, wordId2) ' \
+    query = 'insert into sadnadb.dataMining (bookId, apperanceCount, wordId1, wordId2)' \
             'select wb1.bookId, count(wb1.wordId), wb1.wordId, wb2.wordId ' \
-            'from sadnadb.wordsInBooks wb1, sadnadb.wordsInBooks wb2 ' \
+            'from ' \
+            '(select * from sadnadb.wordsInBooks twb1 where twb1.bookId = %s) wb1, ' \
+            '(select * from sadnadb.wordsInBooks twb2 where twb2.bookId = %s) wb2 ' \
             'where wb1.lineNumber = wb2.lineNumber ' \
             'and wb1.wordId != wb2.wordId ' \
-            'and wb1.bookId = %s and wb2.bookId = %s ' \
             'group by wb1.wordId, wb2.wordId'
     cursor.execute(query, (bookId, bookId))
-
+    rc["db"].commit()
